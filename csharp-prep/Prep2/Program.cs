e873949d07b3d7f655f0dae8d94275e4be468fa8
c-tillett <til18005@byui.edu>
2024-09-19 22:16:00 -0600

    I fixed it

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -10,37 +10,37 @@ class Program
         int percent = int.Parse(input);
         string grade = "";
 
-        if(percent >= 90);
+        if(percent >= 90)
         {
             grade ="A";
         }
-        else if(percent >= 80);
+        else if(percent >= 80)
         {
             grade = "B";
         }
-        else if(percent >= 70);
+        else if(percent >= 70)
         {
             grade = "C";
         }
         
-        else if(percent >= 60);
+        else if(percent >= 60)
         {
             grade = "D";
         }
 
-        else;
+        else
         {
             grade = "F";
         }
 
         Console.WriteLine($"You got a {grade}");
 
-        if (percent >= 70);
+        if (percent >= 70)
         {
             Console.WriteLine("You passed!");
         }
 
-        else;
+        else
 
         {
             Console.WriteLine("You failed...but I believe in you next time!");
