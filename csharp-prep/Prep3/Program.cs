1a8428d77374e65f3d804886fcf89be661540239
c-tillett <til18005@byui.edu>
2024-09-24 12:11:00 -0600

    Changes made

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Console.WriteLine("What is the magic number?");
+        string magic_string = Console.ReadLine();
+        int magic_number = int.Parse(magic_string);
+
+        int correct = 0;
+        while (correct == 0)
+        {
+            Console.WriteLine("What is your guess?");
+            string guess_string = Console.ReadLine();
+            int guess_num = int.Parse(guess_string);
+
+            if (guess_num == magic_number)
+            {
+                correct = 1;
+            }
+            else if (guess_num > magic_number)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else
+            {
+                Console.WriteLine("Higher!");
+            }
+        }
+
+        Console.WriteLine($"That's correct! The magic number is {magic_number}!");
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Diagnostics;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Please enter the first number:");
+        string first_number = Console.ReadLine();
+        int number_one = int.Parse(first_number);
+
+        Console.WriteLine("Please enter the second number:");
+        string second_number = Console.ReadLine();
+        int number_two = int.Parse(second_number);
+        
+        Console.WriteLine("1 = +, 2 = -, 3 = *, and 4 = /");
+        Console.WriteLine("What operation would you like to preform?");
+        string operation = Console.ReadLine();
+        float operater = int.Parse(operation);
+
+        if (operater == 1)
+        {
+            int answer = number_one + number_two;
+            Console.WriteLine($"{number_one} plus {number_two} is {answer}");
+        }
+
+        else if (operater == 2)
+        {
+            int answer = number_one - number_two;
+            Console.WriteLine($"{number_one} minus {number_two} is {answer}");
+        }
+
+        else if (operater == 3)
+        {
+            int answer = number_one * number_two;
+            Console.WriteLine($"{number_one} times {number_two} is {answer}");
+        }
+
+        else if (operater == 4)
+        {
+            float answer = number_one / number_two;
+            Console.WriteLine($"{number_one} divided by {number_two} is {answer}");
+        }
+
+        else
+        {
+            Console.WriteLine("You didn't enter a valid operation! Try running the program again...I didn't want to write a loop.");
+        }
     }
 }
