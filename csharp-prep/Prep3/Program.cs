d32953b812a96b598b6624bce175024443f6db31
c-tillett <til18005@byui.edu>
2024-09-24 12:24:33 -0600

    Changes made

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,13 +6,14 @@ class Program
     {
 
         Console.WriteLine("What is the magic number?");
-        string magic_string = Console.ReadLine();
-        int magic_number = int.Parse(magic_string);
+        Random rng = new Random();
+
+        int magic_number = rng.Next(100);
 
         int correct = 0;
         while (correct == 0)
         {
-            Console.WriteLine("What is your guess?");
+            Console.WriteLine("Guess the magic number! I'll tell you if you need to guess higher or lower.");
             string guess_string = Console.ReadLine();
             int guess_num = int.Parse(guess_string);
 
