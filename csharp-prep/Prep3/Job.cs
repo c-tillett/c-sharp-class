0fee66f31e4864f1ac547540740f3f137638162b
c-tillett <til18005@byui.edu>
2024-09-30 16:04:11 -0600

    Updates made to prep 3 (that file is 10000% in the wrong place)

---
+++ csharp-prep/Prep3/Job.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Job;
+{
+    public string _companyName;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"Spent {_endYear - _startYear} years (from {_startYear} until {_endYear}) as a {_jobTitle} at {_companyName}");
+    }
+}
