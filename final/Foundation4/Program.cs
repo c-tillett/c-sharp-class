3d4f3e2a27ce907dbc7e31cd6845cc00359b82ba
c-tillett <til18005@byui.edu>
2024-12-12 20:51:09 -0700

    One more error that I missed

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         Running running = new Running("Running", "September 21, 2024", 75, 10);
         Biking biking = new Biking("Biking", "December 1, 2024", 15, 10.1);
-        Swimming swimming = new Swimming("Swimming", "July 4, 2024", 60, 10);
+        Swimming swimming = new Swimming("Swimming", "July 4, 2024", 10, 60);
         List<string> summaries = new List<string>{running.GetSummary(), biking.GetSummary(), swimming.GetSummary()};
 
         foreach (string summary in summaries)
